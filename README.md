fea17bb092a7df15cf071c1cd1fa8634c4006a71
DeudlyYT <deudlymandame@gmail.com>
2018-06-09 23:51:20 +0200

    More code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# GamesHost
+# Worldwide Multi-Servers Plan (WMSP)
+
+WSMP software connects to League of Memories central server and runs GameServer instances.
+
+## How does it work?
+
+WSMP doesn't manage lobbies and only runs GameServer instances. So it connects to the LoM central server to retrieve all the info in order to start the game. You can run it without a Key but your server will run as unverified. In order to run a verified server you need a key, which are distributed to servers that have been running for some time and guarantee that they are able to run a full game without issues.
--- WMSP.js
+++ WMSP.js
@@ -1,17 +1,29 @@
 // Write here the IP of the server that is running this
-const myIP = "212.47.249.252";
+const myIP = "";
+/* Although you can run WMSP without using a key, your server will be listed as unverified.
+If you get a key your server will be verified and official */
+const key = "";
 
+/* Here starts the code. Don't touch it if you don't know */
 var Utility = require('./Utility/Utility');
 var CreateFunction = Utility.CreateFunction;
-const mainServerIp = "localhost";
+var WebSocket = require('ws');
+const mainServerIp = "62.4.16.132";
 const mainServerPort = "7778";
 var port = 20000;
 
-var WebSocket = require('ws');
 var ws = new WebSocket("ws://" + mainServerIp + ":" + mainServerPort + "/");
+console.log("WMSP started and connected to main server")
+
+/*ws.onconnect = CreateFunction(this, function(evt){
+    console.log("sending key")
+    send({ message: "Key", key: key })
+})*/
+
 ws.onmessage = CreateFunction(this, function (evt) {
     var received_msg = evt.data;
     console.log("Got message: " + received_msg);
+    send({ message: "Key", key: key })
     var message = JSON.parse(received_msg);
     var messageTitle = message['message'];
     switch (messageTitle) {
@@ -40,7 +52,7 @@ function startGameServer(gameJSON, lobbyID) {
             //if (data.indexOf("Game is ready.") !== -1) {
             console.log("Game is ready, doing callback");
             waitingForBoot = false;
-            send({ message: "PortToUse", port, lobbyID, myIP });
+            send({ message: "PortToUse", port, lobbyID, ip: myIP });
             port++;
         }
 
